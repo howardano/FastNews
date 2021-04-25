d5110b145302093e20437764a5c8cf26030161b6
howardano <58511197+howardano@users.noreply.github.com>
2021-04-25 20:52:13 +0300

    registration and log in.

--- app/src/main/java/ru/mail/park/fastnews/MainActivity.java
+++ app/src/main/java/ru/mail/park/fastnews/MainActivity.java
@@ -1,13 +1,38 @@
 package ru.mail.park.fastnews;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnCreateAcc;
+    ImageView btnlogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCreateAcc = findViewById(R.id.newAccountButton);
+        btnlogIn = findViewById(R.id.loginbtn);
+        btnCreateAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+
+        btnlogIn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LogInActivity.class));
+            }
+        });
     }
 }
