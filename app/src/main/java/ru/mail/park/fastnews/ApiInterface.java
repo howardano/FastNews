3676d21576c209431ec00ddf4b5f5c9e9f19c6ee
howardano <58511197+howardano@users.noreply.github.com>
2021-04-26 20:21:56 +0300

    commit with error.

---
+++ app/src/main/java/ru/mail/park/fastnews/ApiInterface.java
@@ -0,0 +1,25 @@
+package ru.mail.park.fastnews;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import ru.mail.park.fastnews.model.Headlines;
+
+public interface ApiInterface {
+
+
+    @GET("top-headlines")
+    Call<Headlines> getHeadlines(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+    );
+
+    @GET("everything")
+    Call<Headlines> getSpecificData(
+            @Query("q") String query,
+            @Query("apiKey") String apiKey
+    );
+
+
+
+}
